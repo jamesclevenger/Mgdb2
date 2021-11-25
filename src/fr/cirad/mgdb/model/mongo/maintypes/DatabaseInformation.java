512811878ac06cabe913d7f12b7be5300748ff6a
Grégori Mignerot <gregori.mignerot@cirad.fr>
2021-11-25 15:15:54 +0100

    Add the dbInfo collection

---
+++ src/fr/cirad/mgdb/model/mongo/maintypes/DatabaseInformation.java
@@ -0,0 +1,17 @@
+package fr.cirad.mgdb.model.mongo.maintypes;
+
+import java.util.Date;
+
+public class DatabaseInformation {
+	public static final String FIELDNAME_LAST_MODIFICATION = "lastModification";
+	
+	private Date lastModification;
+
+	public Date getLastModification() {
+		return lastModification;
+	}
+
+	public void setLastModification(Date lastModification) {
+		this.lastModification = lastModification;
+	}
+}
